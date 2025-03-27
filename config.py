bbca839a201ac89f69c303200f2510ffc10e64de
Inf3rnus187 <inf3rnus187@gmail.com>
2025-03-27 11:54:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+
 import os
+
 class Config:
     USERNAME = os.getenv('DASH_USER')
     PASSWORD = os.getenv('DASH_PASS')
@@ -9,6 +11,7 @@ class User:
     def __init__(self, username):
         self.id = username
         self.role = 'admin' if username == Config.USERNAME else 'viewer'
+
     def is_authenticated(self): return True
     def is_active(self): return True
     def is_anonymous(self): return False
